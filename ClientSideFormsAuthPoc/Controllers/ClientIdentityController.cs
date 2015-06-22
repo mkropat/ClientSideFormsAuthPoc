706ad8318cfbeb153466c08fc9e8e33000f91cc0
Michael Kropat <michael.kropat@rackspace.com>
2015-06-22 17:55:10 -0400

    Expose identity endpoints

---
+++ ClientSideFormsAuthPoc/Controllers/ClientIdentityController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using System.Web.Security;
+
+namespace ClientSideFormsAuthPoc.Controllers
+{
+    public class ClientIdentityController : ApiController
+    {
+        [HttpPost]
+        public LogInResult LogIn(string username)
+        {
+            var cookie = FormsAuthentication.GetAuthCookie(username, false, "/");
+
+            return new LogInResult
+            {
+                Cookie = cookie.Value,
+                Message = string.Format("User '{0}' has been successfully logged in.", username),
+                Name = cookie.Name,
+                Path = cookie.Path,
+            };
+        }
+
+        public class LogInResult
+        {
+            public string Cookie { get; set; }
+            public string Message { get; set; }
+            public string Name { get; set; }
+            public string Path { get; set; }
+        }
+    }
+}
---
+++ ClientSideFormsAuthPoc/Controllers/ServerIdentityController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using System.Web.Security;
+
+namespace ClientSideFormsAuthPoc.Controllers
+{
+    public class ServerIdentityController : ApiController
+    {
+        [HttpPost]
+        public LogInResult LogIn(string username)
+        {
+            FormsAuthentication.SetAuthCookie(username, false, "/");
+
+            return new LogInResult
+            {
+                Message = string.Format("User '{0}' has been successfully logged in.", username),
+            };
+        }
+
+        public class LogInResult
+        {
+            public string Message { get; set; }
+        }
+    }
+}
